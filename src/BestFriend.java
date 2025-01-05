acff371c7b8f5839620f965948767ef9499a509b
Gayathri <gayathri,qa17@gmail.com>
2025-01-05 12:34:40 +0530

    initial commit

---
+++ src/BestFriend.java
@@ -0,0 +1,6 @@
+public class BestFriend {
+    public static void main(String[] args){
+        System.out.println("Best Friend");
+    }
+    }
+
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public  static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+public class HelloKing{
+    public  static void main(String[] args){
+        System.out.println("Hello King");
+    }
+}
+
